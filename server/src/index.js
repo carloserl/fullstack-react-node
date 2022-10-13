d337048139b6ef251939f6601cbbd8b7f1d3738b
carlos rodriguez <carlosrodriguez@carloss-MacBook-Pro.local>
2022-10-13 15:11:05 -0400

    set redux

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,9 @@
 import express from 'express'
 const app = express()
 import rootRouter from './routes/files.route.js'
-
-
+import cors from 'cors';
+app.use(express.json());
+app.use(cors())
 // routes
 app.use(rootRouter);
 

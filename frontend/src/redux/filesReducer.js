ff3090b77874c5b9450e049ef022ce880044c559
carlos rodriguez <carlosrodriguez@carloss-MacBook-Pro.local>
2022-10-13 21:22:29 -0400

    set redux

--- frontend/src/redux/filesReducer.js
+++ frontend/src/redux/filesReducer.js
@@ -2,34 +2,11 @@ import {createSlice, createReducer } from "@reduxjs/toolkit"
 import { formatRows } from "../utils/utils";
 import { getFileByCVS, getFiles, getFilesList } from "./filesActions"
 
-
 const initialState = {
     rows: [],
     fileList: [],
     selectFile: null,
   }
-
-// const filestSlice = createSlice({
-//     name:"files",
-//     initialState,
-//     extraReducers:{
-//         [getFiles.pending]: (state,action) =>({
-//             ...state,
-//             loading: true,
-//         }),
-//         [getFiles.fulfilled]: (state,action) =>({
-//             ...state,
-//             loading: false,
-//             rows: action.payload
-//         }),
-//         [getFiles.rejected]: (state,action) =>({
-//             ...state,
-//             loading: false,
-//         })
-//     }
-// })
-
-// export default filestSlice.reducer
 
 export const filesReducer = createReducer(initialState, (builder) => {
     builder.addCase(getFiles.pending, (state) => ({
@@ -72,38 +49,8 @@ export const filesReducer = createReducer(initialState, (builder) => {
     
       builder.addCase(getFileByCVS.fulfilled, (state, action) => ({
         ...state,
-        rows: action.payload.message === "OK"?  formatRows([action.payload.data]) :[],
+        rows: action.payload.message === "OK"?  formatRows(action.payload.data) :[],
       }));
     
-  
-  
-    // builder.addCase(postMonstersData.pending, (state) => ({
-    //   ...state,
-    //   result: null,
-    // }));
-  
-    // builder.addCase(postMonstersData.rejected, (state) => ({
-    //   ...state,
-    //   result: null,
-    // }));
-  
-    // builder.addCase(postMonstersData.fulfilled, (state, action) => ({
-    //   ...state,
-    //   result: action.payload,
-    // }));
-  
-  
-    // builder.addCase(setSelectedMonster, (state, action) => ({
-    //   ...state,
-    //   selectedMonster: action.payload,
-    //   result: null
-    // }));
-  
-    // builder.addCase(setSelectedMonsterComputer, (state, action) => ({
-    //   ...state,
-    //   selectedMonsterComputer: action.payload,
-    //   result: null
-    // }));
-  
   });
   
